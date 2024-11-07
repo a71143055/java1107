14012d3fdb8837bc1cccb908904495f4d85e2b79
a71143055 <a71143055@gmail.com>
2024-11-07 11:28:21 +0900

    java1107

---
+++ src/protected1/test/Child.java
@@ -0,0 +1,10 @@
+package protected1.test;
+
+import protected1.Parent;
+
+public class Child extends Parent {
+    public void testPro() {
+        id = "ProtectedId";
+        printProtected();
+    }
+}
