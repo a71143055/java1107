11bf113c92fa4ea882f91fcbdf2964dd1b7e708a
a71143055 <a71143055@gmail.com>
2024-11-07 11:43:27 +0900

    java1107

---
+++ src/ex10_14/MainPro.java
@@ -0,0 +1,10 @@
+package ex10_14;
+
+import ex10_14.pack2.Weapon;
+
+public class MainPro {
+    public static void main(String[] args) {
+        Weapon w = new Weapon("대검", 6000, 50);
+        w.print();
+    }
+}
